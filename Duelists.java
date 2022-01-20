8f31a7f80ee3e5b2eb6b79179c8ed551fd46bdb1
kaitko <95504398+kaitko@users.noreply.github.com>
2022-01-19 21:09:14 -0500

    Add files via upload

--- Duelists.java
+++ Duelists.java
@@ -5,79 +5,91 @@ import java.awt.event.*;
 public class Duelists extends JFrame implements ActionListener {
 
     int level;
+    
+ 
+    //FOR GRAPHICS:
+    JPanel title = new JPanel(new BorderLayout());
+    JPanel displayP = new JPanel(new BorderLayout());
+    
+    JButton display = new JButton("Show game");
+    JLabel text = new JLabel("Duelist");  
+    JLabel displayText = new JLabel("Welcome to Duelist!");
+    Font font2 = new Font("Times New Roman", Font.PLAIN, 20);
+    Font font = new Font("Times New Roman", Font.BOLD, 90);
+    final Color GOLD = new Color(255,204,51);
+    
+    ImageIcon math = new ImageIcon("math2.jpg");
+    JLabel image = new JLabel(math);
 
     JPanel top = new JPanel();
     JPanel mid = new JPanel();
     JPanel gameSelectionPane = new JPanel();
     JPanel gameConfig = new JPanel();
     JPanel gameArea = new JPanel();
-    JButton go = new JButton("Start Game");
+    JButton go = new JButton("Select the Game");
 
     String[] games = {"Mapping", "Factor Duel"};
-    String[][] playerList = {{"Computer", "You"},{"Player 1", "Player 2"}};
-    String[] gameSizeLabel = {"8 x 8", "8 x 9", "8 x 10", "9 x 9", "9 x 10", "9 x 11", "10 x 10", "10 x 11", "11 x 11", "12 x 12", "13 x 13"};
-    int[][]  gameSize      = {{8 , 8} ,{8 , 9} ,{8 , 10} ,{9 , 9} ,{9 , 10} ,{9 , 11} ,{10 , 10} ,{10 , 11}, {11, 11},  {12, 12},  {13, 13}};;
-    String[] levelOptions = {"Easy","Medium","Hard", "Evil"};
-
-    JComboBox cbxGame= new JComboBox(games);
-    JComboBox cbxFirstPlayer = new JComboBox();
-    JComboBox cbxSize = new JComboBox(gameSizeLabel);
-    JComboBox cbxLevel = new JComboBox(levelOptions);
-
     JLabel lblGame = new JLabel("Game:");
-    JLabel lblFirstPlayer = new JLabel("First Player:");
-    JLabel lblSize = new JLabel("Board Size:");
-    JLabel lblLevel = new JLabel("Hardness:");
-    
+    JComboBox cbxGame= new JComboBox(games);
 
     public Duelists() {
         cbxGame.addActionListener(this);
         gameSelectionPane.add(cbxGame);
         gameSelectionPane.setVisible(true);
         go.addActionListener(this);
-        cbxLevel.addActionListener(this);
-        cbxFirstPlayer.setModel(new DefaultComboBoxModel(playerList[cbxGame.getSelectedIndex()]));
-
-        top.add(lblGame);           top.add(gameSelectionPane);
-        top.add(lblFirstPlayer);    top.add(cbxFirstPlayer);
-        top.add(lblSize);           top.add(cbxSize);
-        top.add(lblLevel);          top.add(cbxLevel);
-        top.add(go);
+        top.add(lblGame); top.add(gameSelectionPane);top.add(go);
+        
+      //Graphics:
+        title.setPreferredSize(new Dimension(800,600));
+        title.setBackground(GOLD);
+        
+        text.setFont(font);
+        title.add(text);
+        displayText.setFont(font2);
+        displayP.add(displayText);
+        displayP.add(image);
+        
+        displayP.setPreferredSize(new Dimension(600,500));
+        displayP.add(display, BorderLayout.SOUTH);
+        displayP.setBackground(Color.black);
+        displayText.setForeground(Color.WHITE);
+        
+        title.add(displayP, BorderLayout.SOUTH);
+        display.addActionListener(this);
+        title.setVisible(true);
+        
+        mid.add(title);
 
         setLayout(new BorderLayout());
         add(top, BorderLayout.NORTH);
-        gameArea = new MappingPane(gameSize[cbxSize.getSelectedIndex()][0],gameSize[cbxSize.getSelectedIndex()][1],1,2);
-        //gameArea = new  FactorDuelPane(8,8,1, 2);
-        gameArea.setVisible(true);
+        top.setVisible(false);
+
+        gameArea.setVisible(false);
 
         mid.add(gameArea);
-        mid.setPreferredSize(new Dimension(800, 600));
+        mid.setPreferredSize(new Dimension(800, 650));
         add(mid);
-
         setResizable(false);
 
     }
     public void actionPerformed(ActionEvent e){
-        if (e.getSource()==cbxGame){
-            boolean visible =(cbxGame.getSelectedIndex()==0)? true: false;
-            cbxFirstPlayer.setModel(new DefaultComboBoxModel(playerList[cbxGame.getSelectedIndex()]));
-            lblSize.setVisible(visible);            cbxSize.setVisible( visible);
-            lblLevel.setVisible(visible);           cbxLevel.setVisible(visible);
-        } else if (e.getSource()==cbxLevel){
-            level = cbxLevel.getSelectedIndex()+1;
-            //System.out.printf("You just selected level%d. %n", level);
-        } else if (e.getSource()== go){
-            mid.remove(gameArea);
-            repaint();
-            if (cbxGame.getSelectedIndex()==0){
-                gameArea = new MappingPane(gameSize[cbxSize.getSelectedIndex()][0],gameSize[cbxSize.getSelectedIndex()][1],1,2);
+        if (e.getSource()== go){
+            mid.remove(gameArea); repaint();
+            if (cbxGame.getSelectedIndex()==0) {
+                gameArea = new MappingPane();
             } else {
                 gameArea = new  FactorDuelPane(8,10,1, 2);
-            } 
+            }
+
             gameArea.setVisible(true);
             mid.add(gameArea,BorderLayout.SOUTH);
             pack();
-        } 
+        }
+        else if (e.getSource()== display) {
+        	top.setVisible(true);
+        	title.setVisible(false);
+            gameArea.setVisible(true);
+        }
 
     }
 
--- FactorDuelPane.java
+++ FactorDuelPane.java
@@ -5,69 +5,89 @@ import java.util.concurrent.TimeUnit;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 public class FactorDuelPane extends JPanel implements ActionListener{
-    private JButton[][] grids;
+    private JButton[] btnNumber;
+
     //private int row=0, col=0;
     private ImageIcon[] icon = {new ImageIcon("icon.jpg"), new ImageIcon("player1.jpg"), new ImageIcon("player2.jpg")};
     private int player, level=0;
     private int startPlayer;
     private int rows, cols;
-    private int[][] nextAllowed;
+    private int[] selected;
+    private int[] numOfNextMove;
 
-    
+    private int lastSelection=-2;
+    private int[] strartingNumers = {12,13,14,15,16,18,19,20,21,22,24,25,26,27,28,30,32,33,34,35,36,38,39,40,42,44,45,48,50,52,54,56,60,63,64};
+
     public FactorDuelPane(int rows, int cols, int startPlayer, int level){
         this.rows =rows;
         this.cols =cols;
         this.player= startPlayer;
         this.level=level;
         this.startPlayer= startPlayer;
-        grids = new JButton[rows][cols];
-        nextAllowed = new int[rows][cols];
-        for (int i=0; i<rows;i++) for (int j=0; j<cols;j++) nextAllowed[i][j]=1;
+        btnNumber = new JButton[rows * cols];
+        selected = new int[rows * cols];
+        numOfNextMove= new int[rows * cols];
+        resetNumOfNextMove();
+        
+        // int c = strartingNumers[(int)(Math.random()*(strartingNumers.length))];
+        // selected[c]=1;
+
+        //for (int i=0; i<rows;i++) for (int j=0; j<cols;j++) nextAllowed[i][j]=1;
         setLayout(new GridLayout(rows,cols));
 
-        for (int i = 0; i<rows; i++){
-            for (int j = 0; j<cols; j++){
-                grids[i][j]=new JButton();
-                //if ((i+j)%2!=0) grids[i][j].setBackground(Color.BLACK);
-                grids[i][j].setText(""+(i*cols+j+1));
-                add(grids[i][j]);
-                grids[i][j].addActionListener(this);
-            }
+        for (int i = 0; i<rows * cols ; i++){
+            btnNumber[i]=new JButton();
+            btnNumber[i].setText(""+(i+1));
+            add(btnNumber[i]);
+            btnNumber[i].addActionListener(this);
         }
-        //grids[row][col].setIcon(icon[0]);
         setPreferredSize(new Dimension(50*cols, 50*rows));
         setVisible(true);
+        // select the initial number to start with
+        int c = strartingNumers[(int)(Math.random()*(strartingNumers.length))];
+        System.out.println(c);
+        processClick(c);
+
     }
 
-    private void processClick(int i, int j){
-        if (nextAllowed[i][j]==0) return;
-        try {TimeUnit.MILLISECONDS.sleep(1);} catch (InterruptedException ex) {}
-        grids[i][j].setBackground(Color.BLACK);
-        int hasAllowed =0; 
-        for (int m = 0; m<rows; m++) 
-            for (int n = 0; n<cols; n++) {
-                if (grids[m][n].getBackground()!=Color.black 
-                    && ((m*cols+n+1)%(i*cols+j+1)==0 ||(i*cols+j+1)%(m*cols+n+1)==0))
-                {
-                    nextAllowed[m][n]=1;
-                    grids[m][n].setToolTipText("Allowed");
-                    hasAllowed++;
-
-                } else nextAllowed[m][n]=0;
-            }
-        //grids[i][j].setIcon(icon[player]);
-        //row=i; col=j;
-
-        if (hasAllowed==0) {
+    private void processClick(int i){
+        // if user clicked previously chosen grid 
+        if (selected[i]==1) return;
+        //user clicked a wrong butto, lose 
+        else if ((lastSelection+1)%(i+1) !=0 && (i+1)%(lastSelection+1) != 0) {
+            // handle lose -toDO
+            return;
+        } 
+        // choose a grid that has no further move, winner found 
+        else if (numOfNextMove[i]==0) {
+            btnNumber[i].setBackground(Color.RED);
             showMessageDialog(this, "Player "+player+" won!");
-        } else player=3-player;
+            return;
+        } else {
+            btnNumber[i].setBackground(Color.BLACK);
+            selected[i]=1;
+            resetNumOfNextMove();
+            lastSelection=i;
+            player=3-player;
+        }
+        //try {TimeUnit.MILLISECONDS.sleep(1);} catch (InterruptedException ex) {}
+        repaint();
     }
     
+    private void resetNumOfNextMove(){
+        for (int i =0; i<numOfNextMove.length; i++){
+            numOfNextMove[i]=0;
+            for (int j =0; j<selected.length; j++){
+                if (selected[j]==0 && i != j && ((i+1)%(j+1)==0||(j+1)%(i+1)==0)) numOfNextMove[i]++; 
+            } 
+        }
+
+    }
+
     public void actionPerformed(ActionEvent e){
         Object source = e.getSource();
-        for (int i = 0; i<rows; i++) 
-            for (int j = 0; j<cols; j++) 
-                if (source == grids[i][j]) processClick(i, j);
+        for (int i = 0; i<rows * cols; i++) 
+            if (source == btnNumber[i]) processClick(i);
         return;
     }
     
